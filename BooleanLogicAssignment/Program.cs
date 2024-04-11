38d2a1bc4a1fce1115f9b913521b7bb5be5513c0
NoahKetcham <150949337+NoahKetcham@users.noreply.github.com>
2024-04-11 00:24:36 -0700

    Boolean Logic Submission Assignment

---
+++ BooleanLogicAssignment/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogicAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Car Insuarance Application");
+
+            // Age question
+            Console.Write("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            
+            //Dui Question
+            Console.Write("Have you ever had a DUI? (yes/no)");
+            string duiResponse = Console.ReadLine().Trim().ToLower();
+            bool hasDUI = duiResponse == "yes";
+
+            //Sppeding ticket question
+            Console.Write("How many speeding tickets do you have? ");
+            int speedingTickets = Convert.ToInt32(Console.ReadLine());
+
+            //Qualification
+
+            bool isQualified = age > 15 && !hasDUI && speedingTickets <= 3;
+
+            Console.WriteLine($"Application Approved: {isQualified}");
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ BooleanLogicAssignment/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BooleanLogicAssignment")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BooleanLogicAssignment")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0ed55029-7e99-4167-a832-349c6ab5866b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
